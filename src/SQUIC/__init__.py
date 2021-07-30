2783ca528dd64a21dcd363e07979e68a14a9888a
aryan eftekhari <aryan.eftekhari@usi.ch>
2021-07-30 16:07:04 +0000

    Update __init__.py

--- src/SQUIC/__init__.py
+++ src/SQUIC/__init__.py
@@ -5,41 +5,45 @@
 
 SQUIC is a second-order, L1-regularized maximum likelihood method for performant large-scale sparse precision matrix estimation.
 
+
 #################
 # Usage :
 #################
 
 [X,W,info_times,info_objective,info_logdetX,info_trSX]=SQUIC.run(Y, lambda, max_iter = 100, tol = 1e-3, verbose = 1, M = NULL,  X0 = NULL, W0 = NULL)
+[_,S,info_times,_,_,_]=SQUIC.run(Y, lambda, max_iter = 0)
 
 Arguments:
-    Y:          Input data in the form p (dimensions) by n (samples).
-    l:          Scalar tuning parameter controlling the sparsity of the precision matrix (a non-zero positive number).
-    max_iter:   Maximum number of Newton iterations of the outer loop. Default: 100.
-    tol:        Tolerance for convergence and approximate inversion. Default: 1e-3.
-    verbose:    Level of printing output (0 or 1). Default: 1.
-    M:          The matrix encoding the sparsity pattern of the matrix tuning parameter, i.e., Lambda (p by p). Default: NULL.
-    X0:         Initial guess of the precision matrix (p by p). Default: Identity.
-    W0:         Initial guess of the inverse of the precision matrix (p by p). Default: Identity.
+    Y:          Input data in the formp(dimensions) x n(samples).
+    l:          Scalar tuning parameter a non-zero positive number.
+    max_iter:   Maximum number of Newton iterations.
+    tol:        Tolerance for convergence and approximate inversion.
+    verbose:    Level of printing output (0 or 1).
+    M:          A symmetric sparse (p by p); See note (3) below for details.
+    X0:         Initial value of precision matrix as a sparse matrix (p x p) .
+    W0:         Initial value of inverse precision matrix as a sparse matrix (p x p) ..
 
 NOTE:
 (1) If max_iter=0, the returned value for the inverse of the precision matrix (W) is the sparse sample covariance matrix (S).
 (2) The number of threads used by SQUIC can be defined by setting the enviroment variable OMP_NUM_THREADS (e.g., base> export OMP_NUM_THREADS=12). This may require a restart of the session).
-
+(3) The matrix tuning paramter Lambda:=M_{ij} for M_{ij} != 0 and Lambda_{ij}:=lambda, if otherwise. When M=NULL we have Lambda_{ij}=lambda.
+(4) After importing the package the user must set the path of the shared library libSQUIC.*, using function SQUIC.PATH_TO_libSQUIC().
 
 Return values:
 
- X: Estimated precision matrix (p by p).
- W: Estimated inverse of the precision matrix (p by p).
- info_times: List of different compute times.
+ X: Estimated precision matrix (p x p).
+ W: Estimated inverse of the precision matrix (p x p).
+ S: Sparse (threashold) sample covariance matrix (p x p).
+ info_times: Total runtime.
     [0] info_time_total: Total runtime.
     [1] info_time_sample_cov: Runtime of the sample covariance matrix.
-    [2] info_time_optimize: Runtime of the Newton steps.
-    [3] info_time_factor: Runtime of the Cholesky factorization.
+    [2] info_time_optimize: Runtime of the Newton steps
+    [3] info_time_factor: Runtime of the matrix factorization.
     [4] info_time_approximate_inv: Runtime of the approximate matrix inversion.
     [5] info_time_coordinate_upd: Runtime of the coordinate descent update.
- info_objective: Value of the objective at each Newton iteration.
- info_logdetX: Value of the log determinant of the precision matrix.
- info_trSX: Value of the trace of the sample covariance matrix times the precision matrix.
+ info_objective: Objective value at each Newton iteration.
+ info_logdetX: Log determinant of the estimated precision matrix.
+ info_trSX: Trace of the sample covariance matrix times the estimated precision matrix.
 
 #################
 # References :
